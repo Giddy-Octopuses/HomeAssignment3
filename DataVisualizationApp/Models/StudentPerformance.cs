bad6fdc5d486e6eb7ab329309ed84a94ec33496c
AnnalenaLorenzen <lorenzenannalena@web.de>
2025-03-28 09:05:23 +0100

    data

---
+++ DataVisualizationApp/Models/StudentPerformance.cs
@@ -0,0 +1,23 @@
+public class StudentPerformance
+{
+    public int Hours_Studied { get; set; }
+    public int Attendance { get; set; }
+    public required string Parental_Involvement { get; set; }
+    public required string Access_to_Resources { get; set; }
+    public required string Extracurricular_Activities { get; set; }
+    public int Sleep_Hours { get; set; }
+    public int Previous_Scores { get; set; }
+    public required string Motivation_Level { get; set; }
+    public required string Internet_Access { get; set; }
+    public int Tutoring_Sessions { get; set; }
+    public required string Family_Income { get; set; }
+    public required string Teacher_Quality { get; set; }
+    public required string School_Type { get; set; }
+    public required string Peer_Influence { get; set; }
+    public int Physical_Activity { get; set; }
+    public required string Learning_Disabilities { get; set; }
+    public required string Parental_Education_Level { get; set; }
+    public required string Distance_from_Home { get; set; }
+    public required string Gender { get; set; }
+    public int Exam_Score { get; set; }
+}
