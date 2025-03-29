dfd8aa4a166cea5c81e67c55c5cad71457836e1f
semma7 <emmaj.solyom@gmail.com>
2025-03-29 19:30:02 +0100

    ui first draft

--- DataVisualizationApp/Views/MainWindow.axaml.cs
+++ DataVisualizationApp/Views/MainWindow.axaml.cs
@@ -1,11 +1,33 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 
-namespace DataVisualizationApp.Views;
-
-public partial class MainWindow : Window
+namespace DataVisualizationApp.Views
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+       // private Border mainBorder;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // SetBorderSize();
+        }
+
+       /* private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            mainBorder = this.FindControl<Border>("MainBorder");
+        }
+
+        private void SetBorderSize()
+        {
+            var primaryScreen = Screens.Primary;
+            var screenWidth = primaryScreen.Bounds.Width;
+            var screenHeight = primaryScreen.Bounds.Height;
+
+            mainBorder.Width = screenWidth;
+            mainBorder.Height = screenHeight;
+        } */
     }
 }
