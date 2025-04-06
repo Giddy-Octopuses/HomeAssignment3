d441ee241f31cf5ae402ed65b4eadf126c9e1227
AnnalenaLorenzen <lorenzenannalena@web.de>
2025-04-06 11:52:05 +0200

    chart base

---
+++ DataVisualizationApp/ViewModels/ChartViewModelBase.cs
@@ -0,0 +1,43 @@
+using LiveChartsCore;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace DataVisualizationApp.ViewModels
+{
+    public abstract class ChartViewModelBase : ViewModelBase, INotifyPropertyChanged
+    {
+        private IEnumerable<ISeries> _series = new List<ISeries>(); 
+
+        public IEnumerable<ISeries> Series
+        {
+            get => _series;
+            set
+            {
+                if (_series != value)
+                {
+                    _series = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        // Abstract method for loading data
+        protected abstract void LoadData();
+
+        // INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // Constructor calls LoadData to populate the chart data
+        public ChartViewModelBase()
+        {
+            LoadData();
+        }
+    }
+}
